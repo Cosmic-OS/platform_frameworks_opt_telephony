b25dd540a8a22598826ff834b47a3d3e858e13ff
Harsh Shandilya <msfjarvis@gmail.com>
2016-12-24 10:09:32 +0530

    fix build

--- src/java/com/android/internal/telephony/uicc/UiccController.java
+++ src/java/com/android/internal/telephony/uicc/UiccController.java
@@ -125,7 +125,7 @@ public class UiccController extends Handler {
             mCis[i].registerForIccStatusChanged(this, EVENT_ICC_STATUS_CHANGED, index);
             // TODO remove this once modem correctly notifies the unsols
             if ((DECRYPT_STATE.equals(SystemProperties.get("vold.decrypt")) &&
-                    mCis[i].getRilVersion() >= 9) || radioApmSimNotPwdn) {
+                    (mCis[i].getRilVersion() >= 9) || radioApmSimNotPwdn) {
                 // Reading ICC status in airplane mode is only supported in QCOM
                 // RILs when this property is set to true
                 mCis[i].registerForAvailable(this, EVENT_ICC_STATUS_CHANGED, index);
